619529f44a5690ab73d243106e39069f002e9238
Vikrant Dhongde <vikrantdhongde1@gmail.com>
2026-03-06 17:14:23 +0530

    fix: support multiple CORS origins from config

--- weekly-planner-ui/src/environments/environment.ts
+++ weekly-planner-ui/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5217/api'
+  // apiUrl: 'http://localhost:5217/api'
+  apiUrl: 'https://weekly-planner-api-vikrant-dpczdfbhe9aphkfj.centralindia-01.azurewebsites.net/api'
 };
