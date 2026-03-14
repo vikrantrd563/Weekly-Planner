684e8a306fdc3fe7f4ac174ebe311299efd95423
Vikrant Dhongde <vikrantdhongde1@gmail.com>
2026-03-14 16:52:34 +0530

    fix: restore /apis

--- weekly-planner-ui/src/environments/environment.prod.ts
+++ weekly-planner-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://weekly-planner-api-vrd-c7e6ewbsamfragfp.eastasia-01.azurewebsites.net'
+  apiUrl: 'https://weekly-planner-api-vrd-c7e6ewbsamfragfp.eastasia-01.azurewebsites.net/api'
 };
